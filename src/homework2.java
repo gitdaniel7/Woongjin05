1f0f52d742ffdd7f6402015b80444c072d92719c
gitdaniel7 <wpca4206@gmail.com>
2025-08-11 15:49:41 +0900

    First commit - 프로젝트 업로드

---
+++ src/homework2.java
@@ -0,0 +1,23 @@
+public class homework2 {
+    public static void main(String[] args) {
+        String[] words = {"michelle", "john", "daniel"};
+
+        // compareTo 사용해보기
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = 0; j < words.length - 1 - i; j++) {
+                if (words[j].compareTo(words[j + 1]) > 0) {
+                    // 자리 바꾸기
+                    String temp = words[j];
+                    words[j] = words[j + 1];
+                    words[j + 1] = temp;
+                }
+            }
+        }
+
+        // 결과 출력하는거
+        System.out.println("사전순서로 정렬:");
+        for (String w : words) {
+            System.out.println(w);
+        }
+    }
+}
